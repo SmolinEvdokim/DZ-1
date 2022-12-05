5cb20763292a232146af94cc9e09fbdead05af74
SmolinEvdokim <smolinevdok@gmail.com>
2022-12-06 00:32:40 +0100

    6.2

--- 6/Program.cs
+++ 6/Program.cs
@@ -2,3 +2,17 @@
 //4 -> да
 //-3 -> нет
 // 7 -> нет
+
+Console.Write("Vvedite 4islo: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int remDiv = number % 2;
+
+if(remDiv == 0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
