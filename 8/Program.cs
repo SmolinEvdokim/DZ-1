6f4e0278c030a707b43db74a2a466ba5acbc80ff
SmolinEvdokim <smolinevdok@gmail.com>
2022-12-06 00:33:13 +0100

    8.2

--- 8/Program.cs
+++ 8/Program.cs
@@ -2,3 +2,17 @@
 
 //5 -> 2, 4
 //8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+if(number > 1)
+{
+    while(evenNumber <= number)
+    {
+        Console.Write(evenNumber + " ");
+        evenNumber = evenNumber + 2;
+    }
+}
